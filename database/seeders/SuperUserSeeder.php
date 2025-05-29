62225eb8f3291a47da48572398f73c06944c82d3
fugomika <farrasfadhila76@gmail.com>
2025-05-30 00:57:40 +0700

    Revert "WIP"
    This reverts commit b251b6283785c9de686f8d2395fc27028f19af54.

--- database/seeders/SuperUserSeeder.php
+++ database/seeders/SuperUserSeeder.php
@@ -18,8 +18,8 @@ class SuperUserSeeder extends Seeder
     {
         $user = User::create([
             'name' => 'Administrator',
-            'email' => 'superadmin@gmail.com',
-            'password' => Hash::make("password"),
+            'email' => 'super.admin@test.com',
+            'password' => Hash::make(12345678),
             'is_active' => 1
         ]);
 
